00b53300b011cf29a6229b0582e1d5359f94e438
thomasmmc <thomasmmc@gmail.com>
2016-05-14 15:20:13 -0400

    added @initiative_options for use in view

--- app/controllers/efforts_controller.rb
+++ app/controllers/efforts_controller.rb
@@ -1,6 +1,9 @@
 #require 'httparty'
 
 class EffortsController < ApplicationController
+  def new
+    @initiative_options = Initiative.all.map{ |u| [ u.name, u.ref ] }
+  end
   def submit_efforts
     binding.pry
     @effort
@@ -19,7 +22,7 @@ class EffortsController < ApplicationController
     @server = 'localhost'
     @port = '8088'
     @service = '/services/collector'
-    @api_key = '6578F62B-9D1E-446A-867D-7A09704A6EA7'
+    @api_key = '2D15C2B6-41E4-48BB-A121-166ADFFA0340'
     #Joining the variables to build the url
     @url = 'https://'+@server+':'+@port+@service
     #Building the authstring with the api_key
